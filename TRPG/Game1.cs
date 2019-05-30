cd71641568811831a15fd281e4913ece4ed10bb0
dannamachi <serenita15@gmail.com>
2019-05-30 18:03:10 +1000

    Show inventory on screen
    1. only inventory window
    2. no player items yet

--- TRPG/Game1.cs
+++ TRPG/Game1.cs
@@ -18,8 +18,16 @@ namespace TRPG
         private Player _player;
         private Item _foodChicken;
 
+        private StaticSprite _inventoryWindow;
+        private StaticSprite _inventorySlots;
+        private Button _button;
+        private float _slotX, _slotY;
+
+        private StaticSprite _guibackground;
         private TiledBackground _background;
 
+        private bool _showingInventory;
+        private MouseState _currentMS, _lastMS;
         //properties
         public int ScreenWidth { get => _graphics.PreferredBackBufferWidth; set { _graphics.PreferredBackBufferWidth = value; _graphics.ApplyChanges(); } }
         public int ScreenHeight { get => _graphics.PreferredBackBufferHeight; set { _graphics.PreferredBackBufferHeight = value; _graphics.ApplyChanges(); } }
@@ -28,6 +36,7 @@ namespace TRPG
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+            _showingInventory = false;
         }
         //methods
         /// <summary>
@@ -41,6 +50,9 @@ namespace TRPG
             // TODO: Add your initialization logic here
             _player = new Player();
             _foodChicken = new Item("Chicken");
+            _button = new Button();
+            IsMouseVisible = true;
+            _currentMS = Mouse.GetState();
             base.Initialize();
         }
 
@@ -58,11 +70,30 @@ namespace TRPG
             Texture2D texture = Content.Load<Texture2D>("WalkingWithFlip");
             _player.SetSprite(texture,4,9);
             Texture2D texture2 = Content.Load<Texture2D>("FoodChicken");
-            _foodChicken.Sprite = new StaticSprite(texture2, 0, 0, 100, 100);
+            _foodChicken.Sprite = new StaticSprite(texture2, 0, 0, texture2.Width, texture2.Height);
             _foodChicken.Location = new Vector2(100, 100);
+
+            Texture2D button = Content.Load<Texture2D>("ButtonInventory");
+            _button.SetSprite(button, 0, 0, button.Width, button.Height);
+            _button.Resize(100, 100);
+            _button.Location = new Vector2(ScreenWidth - _button.WidthDrawn, 0);
+
+            Texture2D inventoryWin = Content.Load<Texture2D>("InventoryWindow");
+            Texture2D inventorySlot = Content.Load<Texture2D>("InventorySlots");
+            _inventoryWindow = new StaticSprite(inventoryWin);
+            _inventoryWindow.WidthDrawn = ScreenWidth - _button.WidthDrawn;
+            _inventoryWindow.HeightDrawn = ScreenHeight;
+            _inventorySlots = new StaticSprite(inventorySlot);
+            _inventorySlots.HeightDrawn = _inventoryWindow.HeightDrawn - 150;
+            _slotX = 38;
+            _slotY = 110;
 
             Texture2D texture1 = Content.Load<Texture2D>("Background");
             _background = new TiledBackground(texture1, 7, 8, ScreenWidth, ScreenHeight);
+            Texture2D guibg = Content.Load<Texture2D>("gui_background");
+            _guibackground = new StaticSprite(guibg);
+            _guibackground.WidthDrawn = ScreenWidth;
+            _guibackground.HeightDrawn = ScreenHeight;
         }
 
         /// <summary>
@@ -82,8 +113,21 @@ namespace TRPG
         protected override void Update(GameTime gameTime)
         {
             //getState
+            _lastMS = _currentMS;
             var kstate = Keyboard.GetState();
+            _currentMS = Mouse.GetState();
             float distance = _player.Speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (_currentMS.LeftButton == ButtonState.Released && _lastMS.LeftButton == ButtonState.Pressed)
+            {
+                if (_button.IsPressed(_lastMS.Position)) { _showingInventory = !_showingInventory; }
+            }
+
+            if (_showingInventory)
+            {
+                if (kstate.IsKeyDown(Keys.A)) { if (_slotX + _inventorySlots.WidthDrawn > 676) { _slotX -= 10; } }
+                if (kstate.IsKeyDown(Keys.D)) { if (_slotX < 38) { _slotX += 10; } }
+            }
 
             if (kstate.IsKeyDown(Keys.X)) { _player.Resize(215, 165); }
             if (kstate.IsKeyDown(Keys.Z)) { _player.Resize(107, 83); }
@@ -167,12 +211,23 @@ namespace TRPG
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
             // TODO: Add your drawing code here
-            _background.Draw(_spriteBatch);
-            if (!_player.Have("Chicken"))
-                _foodChicken.Draw(_spriteBatch);
-            _player.Draw(_spriteBatch);
+            if (!_showingInventory)
+            {
+                _background.Draw(_spriteBatch);
+                _button.Draw(_spriteBatch);
+                if (!_player.Have("Chicken"))
+                    _foodChicken.Draw(_spriteBatch);
+                _player.Draw(_spriteBatch);
+            }
+            else
+            {
+                _guibackground.Draw(_spriteBatch, new Vector2(0,0));
+                _button.Draw(_spriteBatch);
+                _inventorySlots.Draw(_spriteBatch, new Vector2(_slotX,_slotY));
+                _inventoryWindow.Draw(_spriteBatch, new Vector2(0, 0));
+
+            }
 
             base.Draw(gameTime);
         }
---
+++ TRPG/src/Button.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TRPG.src
+{
+    public class Button
+    {
+        //fields
+        private StaticSprite _sprite;
+        //properties
+        public Rectangle ButtonRect { get => new Rectangle((int)Location.X, (int)Location.Y, WidthDrawn, HeightDrawn); }
+        public int WidthDrawn { get => _sprite.WidthDrawn; }
+        public int HeightDrawn { get => _sprite.HeightDrawn; }
+        public Vector2 Location { get; set; }
+        public string Text { get; set; }
+        //constructors
+        public Button()
+        {
+            Location = new Vector2(0, 0);
+        }
+        //methods
+        public bool IsPressed(Point mousept)
+        {
+            return ButtonRect.Contains(mousept);
+        }
+        public void Resize (int width, int height)
+        {
+            _sprite.WidthDrawn = width;
+            _sprite.HeightDrawn = height;
+        }
+        public void SetSprite(Texture2D texture, int horiDist, int vertDist, int width, int height)
+        {
+            _sprite = new StaticSprite(texture, horiDist,vertDist,width,height);
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            if (_sprite != null) { _sprite.Draw(spriteBatch, Location); }
+        }
+    }
+}
