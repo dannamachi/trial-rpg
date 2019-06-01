e983da17c862ba1a284f5cdf3f1fd29ada073572
dannamachi <serenita15@gmail.com>
2019-06-01 15:11:10 +1000

    Allow different kinds of event

--- TRPG/src/InteractiveRect.cs
+++ TRPG/src/InteractiveRect.cs
@@ -6,39 +6,62 @@ namespace TRPG.src
 {
     public class InteractiveRect
     {
-        //private List<Button> _buttons;
-        private Button _button;
+        private Dictionary<Button, ClickEvent> _buttonDict;
+        private Dictionary<string, ClickEvent> _eventDict;
 
         public StaticSprite Sprite { get; set; }
 
-        public InteractiveRect(Texture2D texture, int widthDrawn, int heightDrawn, Button button)
+        public InteractiveRect(Texture2D texture, int widthDrawn, int heightDrawn)
         {
-            _button = button;
+            _buttonDict = new Dictionary<Button, ClickEvent>();
+            _eventDict = new Dictionary<string, ClickEvent>();
             Sprite = new StaticSprite(texture);
             Sprite.WidthDrawn = widthDrawn;
             Sprite.HeightDrawn = heightDrawn;
         }
 
+        private bool HasEvent(string eventname)
+        {
+            foreach (string name in _eventDict.Keys)
+            {
+                if (name == eventname) { return true; }
+            }
+            return false;
+        }
+
+        public void SetButton(Button button, string eventname)
+        {
+            if (HasEvent(eventname)) {
+                _buttonDict[button] = _eventDict[eventname];
+            }
+        }
+
         public void Draw(SpriteBatch spriteBatch, Vector2 location)
         {
             Sprite.Draw(spriteBatch, location);
-            _button.Draw(spriteBatch);
+            foreach (Button button in _buttonDict.Keys)
+            {
+                button.Draw(spriteBatch);
+            }
         }
 
         public delegate void ClickEvent();
 
         public event ClickEvent ClickQuit;
 
-        protected void OnClickQuit()
-        {
-            ClickQuit?.Invoke();
-        }
+        //protected void OnClickQuit()
+        //{
+        //    ClickQuit?.Invoke();
+        //}
 
         public void CheckEvent(Point mousept)
         {
-            if (_button.IsPressed(mousept))
+            foreach (Button button in _buttonDict.Keys)
             {
-                OnClickQuit();
+                if (button.IsPressed(mousept))
+                {
+                    _buttonDict[button]?.Invoke();
+                }
             }
         }
     }
