b9f5444e446ca9c5bcc947f2cfd2f0384a5ca433
dannamachi <serenita15@gmail.com>
2019-10-24 00:18:19 +1100

    Fix indentation + fulfill quest func for FinishQuest Trigger

--- src/Model/TriggerF.cs
+++ src/Model/TriggerF.cs
@@ -3,26 +3,26 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace SEVirtual {
-  public class TriggerF : Trigger {
-    //fields
-    private List<string> _questNames;
-    //constructors
-    public TriggerF(List<string> questNames) : base() {
-      _questNames = new List<string>();
-      foreach (string name in questNames) {
-        _questNames.Add(name);
-      }
-    }
-    //properties
-    //methods
-    protected override void PerformFlip(Player p) {
-      foreach (string questName in _questNames) {
-        if (p.Has(questName,"Q")) {
-          if (p.Find(questName,"Q").IsFulfilledBy(p)) {
-            p.Complete(questName);
-          }
+    public class TriggerF : Trigger {
+        //fields
+        private List<string> _questNames;
+        //constructors
+        public TriggerF(List<string> questNames) : base() {
+            _questNames = new List<string>();
+            foreach (string name in questNames) {
+                _questNames.Add(name);
+            }
         }
-      }
+        //properties
+        //methods
+        protected override void PerformFlip(Player p) {
+            foreach (string questName in _questNames) {
+                if (p.Has(questName,"Q")) {
+                    if ((p.Find(questName,"Q") as Quest).IsFulfilledBy(p)) {
+                        p.Complete(questName);
+                    }
+                }
+            }
+        }
     }
-  }
 }
