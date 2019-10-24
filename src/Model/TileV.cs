114d12761f87efe6de289e3f5769af775cb646c5
dannamachi <serenita15@gmail.com>
2019-10-24 19:17:07 +1100

    Update constructors and loc properties for TileV

--- src/Model/TileV.cs
+++ src/Model/TileV.cs
@@ -11,23 +11,36 @@ namespace SEVirtual {
     }
     public class TileV {
         //fields
+        private int _x;
+        private int _y;
         private Dictionary<TDir,TileV> _tileDict;
         //constructors
-        public TileV(Trigger trig, Dictionary<TDir,TileV> tileDict, bool canMoveTo, int x, int y) {
+        public TileV(Trigger trig, bool canMoveTo, int x, int y) {
             Trigger = trig;
             Blocked = canMoveTo;
-            _tileDict = tileDict;
-            X = x;
-            Y = y;
+            _x = x;
+            _y = y;
+            _tileDict = new Dictionary<TDir, TileV>();
         }
-        public TileV(int x, int y) : this(null,null,false,x,y) {}
-        public TileV(Dictionary<TDir,TileV> tileDict,int x,int y) : this(null,tileDict,true,x,y) {}
         //properties
-        public int X { get; }
-        public int Y { get; }
+        public int X { get => _x; }
+        public int Y { get => _y; }
         public Trigger Trigger { get;set; }
         public bool Blocked { get;set; }
         //methods
+        public bool IsAt(int x, int y)
+        {
+            return _x == x && _y == y;
+        }
+        public void LinkTileDict(Dictionary<TDir,TileV> dict)
+        {
+            _tileDict = dict;
+        }
+        private void LinkTileV(TileV tileV,TDir dir)
+        {
+            if (tileV != null)
+                _tileDict[dir] = tileV;
+        }
         public TileV GetNextTile(TDir dir) {
             return _tileDict[dir];
         }
